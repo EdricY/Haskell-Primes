343aed6923d588361c83de6664adffb66c96dd0a
EdricY <yu.edric@gmail.com>
2019-04-30 02:26:27 -0400

    do rpn stuff

--- primes.hs
+++ primes.hs
@@ -73,3 +73,94 @@ lowerPartition pivot xs =
 upperPartition :: Int -> [Int] -> [Int]
 upperPartition pivot xs =
   filter (>= pivot) xs
+
+evalrpn :: String -> Int
+evalrpn s = evalrpn2 (mywords s) []
+
+  
+evalrpn2 :: [String] -> [Int] -> Int
+evalrpn2 [] ns = head ns
+evalrpn2 (x:xs) ns
+  | isdigit (head x) = evalrpn2 xs (xint:ns)
+  | x == "+" = evalrpn2 xs ((n1+n2):ttns)
+  | x == "*" = evalrpn2 xs ((n1*n2):ttns)
+  where
+    xint = read x :: Int
+    tns = tail ns
+    n1 = head ns
+    n2 = head tns
+    ttns = tail tns
+
+infix2rpn :: String -> String
+infix2rpn s = myunwords (parseExpr (mywords s))
+
+parseExpr :: [String] -> [String]
+parseExpr words = 
+  term ++ (parseTerms rem)
+  where
+    term = parseTerm words
+    rem = drop (length term) words
+
+parseTerms :: [String] -> [String]
+parseTerms [] = []
+parseTerms (lookahead:words)
+  | lookahead == "+" = term 
+                        ++ ["+"]
+                        ++ parseTerms rem
+  | otherwise = []
+  where
+    term = parseTerm words
+    rem = drop (length term) words
+
+parseTerm :: [String] -> [String]
+parseTerm words = 
+  factor ++ (parseFactors rem)
+  where
+    factor = parseFactor words
+    rem = drop (length factor) words
+
+parseFactors :: [String] -> [String]
+parseFactors [] = []
+parseFactors (lookahead:words)
+  | lookahead == "*" = factor 
+                        ++ ["*"]
+                        ++ parseFactors rem
+  | otherwise = []
+  where
+    factor = parseFactor words
+    rem = drop (length factor) words
+
+parseFactor :: [String] -> [String]
+parseFactor (lookahead:words)
+  | isdigit (head lookahead) = [lookahead]
+  | lookahead == "(" = ["?"] ++ (parseExpr words) ++ ["?"]
+  -- two extra elements makes the previous call drop the parentheses
+  | otherwise = []
+
+myunwords :: [String] -> String
+myunwords [x] = x
+myunwords (x:xs)
+  | x == "?" = myunwords xs
+  | otherwise = x ++ " " ++ myunwords xs
+
+isparen c = (c == '(' || c == ')')
+isop    c = (c == '+' || c == '*')
+isdigit c = (c >= '0' && c <= '9')
+
+-- from Dr. Denning
+mywords :: String -> [String]
+mywords s = parser [] s
+  where
+    -- readint reads digits until non-digit
+    -- returns ("digit","remainder of string")
+    readint "" = ("","")
+    readint (h:t)
+      | (isdigit h) = (h:(fst (readint t)), snd (readint t))
+      | otherwise  = ("", h:t)
+    parser l "" = l
+    parser l (h:t)
+      | (isdigit h) = parser (l ++ [fst (readint (h:t))])
+                              (snd (readint (h:t)))
+      | (isop h)    = parser (l ++ [h:""]) t
+      | (isparen h) = parser (l ++ [h:""]) t
+      | h == ' '    = parser l t
